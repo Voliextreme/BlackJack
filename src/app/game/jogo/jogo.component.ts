39824e4d8768dd85e08e6fc4083c070a2ac278f3
Voliextreme <voliextreme@gmail.com>
2021-06-22 22:50:26 +0100

    commented

--- src/app/game/jogo/jogo.component.ts
+++ src/app/game/jogo/jogo.component.ts
@@ -10,17 +10,18 @@ import { SaldoService } from 'src/app/services/saldo.service';
   styleUrls: ['./jogo.component.css']
 })
 export class JogoComponent implements OnInit {
-
+  //Importar Serviços 
   saldo : SaldoService
   constructor(private getDeck: BlackjackService, private getSaldo : SaldoService) { 
     this.saldo = getSaldo;
   }
 
-
+  //Ao abrir a pagina chamar a funçao start() 
   ngOnInit(): void {
     this.start();
   }
 
+  // Apos o onInit esta funçao é executada e guarda os HTMLElemnts num objeto cartas2
   ngAfterViewInit() : void{
     this.cartas2.push(this.card0);
     this.cartas2.push(this.card1);
@@ -35,6 +36,7 @@ export class JogoComponent implements OnInit {
 
   }
 
+  // ViewChield para manipulaçao do html e css 
   @ViewChild('card0') card0 : any;
   @ViewChild('card1') card1 : any;
   @ViewChild('card2') card2 : any;
@@ -46,19 +48,36 @@ export class JogoComponent implements OnInit {
   @ViewChild('card8') card8 : any;
   @ViewChild('card9') card9 : any;
 
-  // Verificar se é necessario
-  @ViewChild('dealer') dealer : any;
-  @ViewChild('player') player : any;
-
   @ViewChild('startdiv') startdiv : any;
 
+  @ViewChild('decisao') decisao : any;
+  @ViewChild('result') result : any;
+  @ViewChild('h1') h1 : any;
+  @ViewChild('page') page : any;
+
+
+  //variaveis inicializadas 
+  contador = 0;
+  pontosPlayer = 0;
+  pontosDealer = 0;
+  showChips : boolean = false;
+  verify :any;
+  stayoption : boolean;
+  aposta : any = 0;
+
+  //Iniciar um objeto cartas2 
   cartas2 = [];
+  // igualar o deckid ao campo da class Deckinit
   deckid ?: Deckinit
 
+  //Executa a funçao start
   start(){
+  //chama a funçao getDeck do servico e da subscribe nos parametros recebido da api
   this.getDeck.getdeck().subscribe(
     x =>{
+      //guarda o deckId
      this.deckid = new Deckinit(x);
+     //chama a funçao getDeckCards 
      this.getDeckCards();
      console.log(this.deckid)
     })
@@ -66,6 +85,7 @@ export class JogoComponent implements OnInit {
 
   // Get de cartas, converaso de dados para Card(y)
   cartasUso : Array<Card> = [];
+  // funçao para ir buscar á api as cartas e guardar num objeto cartasUso
   getDeckCards(){
     if(this.deckid)
       this.getDeck.getCards(this.deckid.deck_id).subscribe(
@@ -76,10 +96,8 @@ export class JogoComponent implements OnInit {
   //explicar que o maximo sao 5 cartas no relatório
 
 
-  contador = 0;
-  pontosPlayer = 0;
-  pontosDealer = 0;
-
+  
+  //Metodo de verificaçao de todas as possibilidades de vitoria , derrota ou empate
   verificar(){
     if (this.pontosPlayer > 21) {
       return 'loser'
@@ -99,7 +117,7 @@ export class JogoComponent implements OnInit {
     return "0";
   }
 
-  showChips : boolean = false;
+  // Ao clickar no botao "Start" o jogo  vai começar e o mesmo é ocultado com display none
   play(){
     this.showChips = true;
     this.startdiv.nativeElement.style.display = "none";
@@ -108,16 +126,18 @@ export class JogoComponent implements OnInit {
 
   }
 
-  @ViewChild('decisao') decisao : any;
+  //Funçao para começar a dar as cartas no qual ordenadamente o player e o dealer recebem duas cartas sendo que uma das do dealer fica virada para baixo
   givecards(){
-    
     if(this.contador != 3){
+      //mostra a face da carta
       this.cartas2[this.contador].nativeElement.src = this.cartasUso[this.contador].image;
       this.cartas2[this.contador].nativeElement.style.display = "block";
+      //Condiçao para verificar se a carta que o "jogadorAtual" receber um "As" e os pontos somados ultrapassarem 21 entao o "As" vale 1 ponto - Segundo Regras Oficiais
       if(this.contador % 2 == 0){
         if(parseInt(this.cartasUso[this.contador].value) == 11 && (parseInt(this.cartasUso[this.contador].value) + this.pontosPlayer) > 22){
           this.cartasUso[this.contador].value = "1";
         }
+        //incrementar os pontos do Player
         this.pontosPlayer += parseInt(this.cartasUso[this.contador].value);
       }else{
         if(parseInt(this.cartasUso[this.contador].value) == 11 && (parseInt(this.cartasUso[this.contador].value) + this.pontosPlayer) > 22){
@@ -126,10 +146,12 @@ export class JogoComponent implements OnInit {
         this.pontosDealer += parseInt(this.cartasUso[this.contador].value);
       }
       this.contador++;
+      // uma pequena pausa de 600 milisegundos exclusivamente para aspectos visuais e logicos
       setTimeout(() => {
         this.givecards();
       }, 600);
     }else {
+      //caso a proxima carta a ser dada seja a "quarta" ou seja a segunda carta do dealer apenas mostra o verso
       this.cartas2[this.contador].nativeElement.src = "../../../assets/Images/card.png";
       this.cartas2[this.contador].nativeElement.style.display = "block";
       this.decisao.nativeElement.style.display = "flex";
@@ -137,6 +159,7 @@ export class JogoComponent implements OnInit {
     }
   };
 
+  //Caso o jogador escolha "hit" vai lhe ser dada uma nova carta, se o jogador tiver blackjack, ou seja, 21 pontos nao podera pedir uma carta
   hit(){
     if(this.pontosPlayer < 21){
       this.cartas2[this.contador].nativeElement.src = this.cartasUso[this.contador].image;
@@ -156,13 +179,11 @@ export class JogoComponent implements OnInit {
     }
   }
 
-  @ViewChild('result') result : any;
-  @ViewChild('h1') h1 : any;
-  @ViewChild('page') page : any;
 
-  verify :any;
+  //Caso o jogador escolha "stay" ou se o player nao ultrapassar os 22 pontos a jogada passa para o dealer
 dealerCards(){
   if(this.pontosPlayer < 22 || this.stayoption == true){
+    //se o dealer tiver 16 ou menos pontos e ainda nao tiver ganho ao player , é obrigatorio ele receber uma nova carta - Regras oficiais
     if(this.pontosDealer <= 16 && this.pontosDealer < this.pontosPlayer || this.pontosDealer <= 16 && this.pontosDealer == this.pontosPlayer){
       this.cartas2[this.contador].nativeElement.src = this.cartasUso[this.contador].image;
       this.cartas2[this.contador].nativeElement.style.display = "block";
@@ -174,10 +195,12 @@ dealerCards(){
       setTimeout(() => {
         this.dealerCards();
       }, 600);
+      //caso o if em cima ja nao se verificar, iremos chamar a funçao para saber quem foi o vencedor e passamos para o fim da jogada
     }else{
       this.verify  = this.verificar();
       this.fim(this.verify);
     }
+    //Caso o jogador ultrapasse os 22 pontos a ultima carta do dealer é mostrada 
   }else{
     this.cartas2[this.contador].nativeElement.src = this.cartasUso[this.contador].image;
     this.cartas2[this.contador].nativeElement.style.display = "block";
@@ -187,13 +210,14 @@ dealerCards(){
   
 }
 
-stayoption : boolean;
+// Caso o player escolha ficar com as cartas que tem a jogada passa para o dealer e o contador é ajustado ás cartas do dealer
 stay(){
   this.stayoption = true;
   this.contador = 3;
   this.dealerCards();
 }
 
+// Na funçao fim é mostrado o resultado da jogada e o valor da aposta ganhou ou perdido respectivamente
 fim(verify){
   this.result.nativeElement.style.display = "flex";
   if(verify == "winner"){
@@ -211,6 +235,7 @@ fim(verify){
   }
 }
 
+//Reset é a funçao para igualar todas as variaveis necesarias e esconder as cartas para voltar a fazer uma nova jogada 
 reset(){
   setTimeout(() => {
     this.contador = 0;
@@ -228,13 +253,14 @@ reset(){
   }, 600);
 }
 
-aposta : any = 0;
+// O player pode apostar as fichas que tem 
 aumentarAposta(valor : any){
   if(this.aposta + valor <= this.saldo.Playersaldo.saldo){
     this.aposta += Number(valor);
   }
 }
 
+// ---------------------------------------------------------------------Alterar -------------------------------- Sair para o menu principal.
 leave(){
   console.log(this.getSaldo.Playersaldo.saldo);
 }
